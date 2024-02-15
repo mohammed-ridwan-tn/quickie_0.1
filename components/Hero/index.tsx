eadaf0b2eda755ce8311df1a95b3d6c5a39493fc
ridwan <ridwan@webandcrafts.in>
2024-02-15 11:55:14 +0530

    fix

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                  Your Protection, Our Priority
+                  Your Protection- Our Priority
                 </h1>
                 <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
                   Startup is free Next.js template for startups and SaaS
